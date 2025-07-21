4737c1e40318347a03725d5c5318e58e6adf88d3
Mayanna Oliveira <mayannait@gmail.com>
2025-07-20 21:55:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ async function updateGist(stats) {
         ]
             .map((content) => {
                 let line = `${content[1]}:${content[2]}`;
-                line = line.replace(':', ':' + ' '.repeat(45 - line.length));
+             // line = line.replace(':', ':' + ' '.repeat(45 - line.length));
+                line = line.replace(':', ':' + ' '.repeat(35 - line.length));
                 line = `${content[0]}    ${line}`;
                 return line;
             })
